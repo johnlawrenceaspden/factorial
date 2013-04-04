48a60088a159314c67ea7a9191116e6c4f217e36
John Lawrence Aspden <github@aspden.com>
2013-04-04 21:49:18 +0100

    extra instructions for copy to clojars via scp

--- project.clj
+++ project.clj
@@ -4,3 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
+
+;; either lein deploy clojars (in which case it will bother you about passwords), or 
+
+;; lein jar
+;; lein pom
+;; scp pom.xml target/factorial-0.1.0-SNAPSHOT.jar clojars@clojars.org:
+
+;; to copy it over using your ssh public key
